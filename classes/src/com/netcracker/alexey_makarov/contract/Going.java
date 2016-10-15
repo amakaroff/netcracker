a1f8f8b70f06052ebd5b6542f777a863b1f7f5f3
Alexey Makarov <alekseymakarov1996@gmail.com>
2016-10-15 22:02:49 +0300

    Revert "production"
    This reverts commit fc650231d6bfca11bcd529b1e04384a1a370aab2.

--- classes/src/com/netcracker/alexey_makarov/contract/Going.java
+++
@@ -1,9 +0,0 @@
-package com.netcracker.alexey_makarov.contract;
-
-/**
- * Created by Алексей on 15.10.2016.
- */
-public interface Going {
-
-    void go();
-}
--- classes/src/com/netcracker/alexey_makarov/view/printer/Printer.java
+++
@@ -1,9 +0,0 @@
-package com.netcracker.alexey_makarov.view.printer;
-
-/**
- * Created by Алексей on 15.10.2016.
- */
-public interface Printer {
-
-    void print(String data);
-}
